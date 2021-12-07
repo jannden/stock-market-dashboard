c458709197166b0c1f900f82da6112ca86e2e85b
Teya <andreevateya@gmail.com>
2021-12-07 20:55:06 +0200

    Test

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const title = "React with: ";
+const title = "Final React Project with: ";
 const tech = [
   "Babel",
   "Webpack 5",
