a7401399094c990073b890b3b72f9368b7f3ec76
jan-cibulka <jc@pntr.eu>
2021-12-12 11:12:37 +0200

    Header Component done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
 
+// Components
+import Header from "./components/Header";
+
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const App = function App({ title, tech }) {
   const [clicks, changeClicks] = useState(0);
 
@@ -7,6 +13,7 @@ const App = function App({ title, tech }) {
 
   return (
     <div>
+      <Header />
       <strong>{title}</strong>
       <ul>{techList}</ul>
       <button type="button" onClick={() => changeClicks(() => clicks + 1)}>
