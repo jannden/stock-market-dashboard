1581d031b132f525aec39a9565d10ac6fa48cf6b
jan-cibulka <jc@pntr.eu>
2021-12-23 17:25:53 +0200

    Firestore implemented -> now we can add custom data to user profiles.

--- src/helpers/AuthContext.jsx
+++ src/helpers/AuthContext.jsx
@@ -13,7 +13,8 @@ import {
   sendPasswordResetEmail,
   EmailAuthProvider,
 } from "firebase/auth";
-import { auth } from "./firebase";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+import { auth, db } from "./firebase";
 
 // Creating context for authentication toolkit
 const AuthContext = React.createContext();
@@ -21,7 +22,12 @@ const AuthContext = React.createContext();
 // Custom hook to wrap authentication context
 export const AuthProvider = function AuthProvider({ children }) {
   // State for user data
-  const [currentUser, setCurrentUser] = React.useState();
+  const [currentUser, setCurrentUser] = React.useState({
+    uid: null,
+    displayName: null,
+    email: null,
+    photoURL: null,
+  });
 
   // Since Firebase handles the local storage and setting user data, we have to wait for them to be ready
   // So we create a loading state to prevent undefined errors
@@ -34,6 +40,7 @@ export const AuthProvider = function AuthProvider({ children }) {
       onAuthStateChanged(auth, (user) => {
         if (user) {
           const data = {
+            uid: user.uid,
             displayName: user.displayName,
             email: user.email,
             photoURL: user.photoURL,
@@ -61,6 +68,27 @@ export const AuthProvider = function AuthProvider({ children }) {
   const handleResetPassword = (email) =>
     sendPasswordResetEmail(auth.currentUser, email);
 
+  const createFirestore = async (newData) => {
+    try {
+      await setDoc(doc(db, "users", currentUser.uid), newData);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
+  const getFirestore = async () => {
+    try {
+      const docRef = doc(db, "users", currentUser.uid);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        return docSnap.data();
+      }
+      return { error: "User not found..." };
+    } catch (error) {
+      return { error };
+    }
+  };
+
   // We will export the toolkit
   const authToolkit = {
     currentUser,
@@ -73,6 +101,8 @@ export const AuthProvider = function AuthProvider({ children }) {
     handleUpdatePassword,
     handleResetPassword,
     EmailAuthProvider,
+    createFirestore,
+    getFirestore,
   };
 
   // We wrap the children in the context if the loading phase finished and provide the children with our authentication toolkit
