0c4b115aa29f6872e788737685f84afcd7f0a321
jan-cibulka <jc@pntr.eu>
2022-01-03 19:03:49 +0200

    Jest and Enzyme set up.

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ import rootReducer from "./reducers";
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
+    applyMiddleware(thunk), // Thunk is necessary for async action creators
     window.__REDUX_DEVTOOLS_EXTENSION__
       ? window.__REDUX_DEVTOOLS_EXTENSION__()
       : (f) => f // This is to make the Redux for Chrome extension work in an incognito window with disabled Redux extension
