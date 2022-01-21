09aec78e91f06f6a4df8f9b933574a99998fea32
jan-cibulka <jc@pntr.eu>
2022-01-21 17:09:55 +0200

    CI for tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   setupFilesAfterEnv: ["./enzyme.js"],
+  reporters: ["default", "jest-junit"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<root>/__mocks__/fileMock.js",
